3982cc1c4d25f85014c077b07ecba5b6f6d029e0
Nogi Ragil Triwardana <nogir16111998@gmail.com>
2022-08-18 20:35:56 +0700

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '/styles/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -15,6 +16,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
 
         <p className={styles.description}>
           Get started by editing{' '}
