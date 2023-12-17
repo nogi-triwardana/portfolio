ab0fac31d7d4a24fcca4aca63bac973257b84357
nogi.triwardana <nogi.triwardana@smt-id.com>
2023-12-17 17:30:36 +0700

    ✨ implement event ga to certificate and contact

--- components/partials/contact/index.tsx
+++ components/partials/contact/index.tsx
@@ -5,6 +5,14 @@ import _ from 'lodash';
 const Contact = forwardRef(function Contact(props: any, ref: React.Ref<HTMLDivElement>) {
   const { contact, isDarkMode } = useContext(LayoutContext);
 
+  const sendEventClickContactHandler = (param: any) => {
+    if(typeof window !== "undefined") {
+      window.gtag("event", "click_contact", {
+        category_contact: param.name
+      })
+    }
+  }
+
   return (
     <div className={`relative ${isDarkMode ? `bg-[#424543]` : `bg-[#f7f7f5]`}`}>
       <div className={`flex flex-col justify-center w-full space-y-8 relative py-16`} ref={ref}>
@@ -15,7 +23,13 @@ const Contact = forwardRef(function Contact(props: any, ref: React.Ref<HTMLDivEl
         </div>
         <div className={`grid grid-cols-4 pb-16 w-2/3 h-fit mx-auto justify-center ${isDarkMode ? `text-[#dce3de]` : `text-paletteText-primary`}`}>
           {_.map(contact, (item: any, key: any) => (
-            <a href={item?.link} target="_blank" className={`flex justify-center focus:outline-none h-fit`} key={'CONTACT-' + key}>
+            <a 
+              href={item?.link} 
+              target="_blank" 
+              className={`flex justify-center focus:outline-none h-fit`} 
+              key={'CONTACT-' + key}
+              onClick={() => sendEventClickContactHandler(item)}
+            >
               <div className={`text-4xl sm:text-6xl`}> 
                 {item?.icon}
               </div>
--- constants/contact-constants.tsx
+++ constants/contact-constants.tsx
@@ -5,18 +5,22 @@ import { FaWhatsappSquare } from 'react-icons/fa';
 export const contactConstant = [
   {
     link: 'https://www.instagram.com/nogitriwardana_/',
+    name: 'instagram',
     icon: <AiFillInstagram />
   },
   {
     link: 'https://www.linkedin.com/in/nogi-ragil-triwardana-a3a66b198/',
+    name: 'linkedin',
     icon: <AiFillLinkedin />
   },
   {
     link: 'mailto:nogitriwardana@gmail.com',
+    name: 'email',
     icon: <MdEmail />
   },
   {
     link: 'https://api.whatsapp.com/send/?phone=6285318909969&type=phone_number&app_absent=0',
+    name: 'whatsapp',
     icon: <FaWhatsappSquare />
   }
 ]
