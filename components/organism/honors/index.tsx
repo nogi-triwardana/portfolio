a86ab8d52cc9863805082751bee94049661486f5
Nogi Ragil Triwardana <nogi.triwardana@smt-id.com>
2024-12-28 13:38:10 +0700

    💄feat(ui): slicing selected section at button navbar

--- components/organism/honors/index.tsx
+++ components/organism/honors/index.tsx
@@ -63,7 +63,7 @@ const Honors = forwardRef(function Honors(props: any, ref: React.Ref<HTMLDivElem
         xmlns="http://www.w3.org/2000/svg" 
         viewBox="0 0 1440 320"
       >
-        <path fill={`${isDarkMode ? `#1f0a4d` : `#d1d0cd`}`} fill-opacity="1" d="M0,96L34.3,90.7C68.6,85,137,75,206,58.7C274.3,43,343,21,411,21.3C480,21,549,43,617,96C685.7,149,754,235,823,256C891.4,277,960,235,1029,218.7C1097.1,203,1166,213,1234,234.7C1302.9,256,1371,288,1406,304L1440,320L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path>
+        <path fill={`${isDarkMode ? `#1f0a4d` : `#d1d0cd`}`} fillOpacity="1" d="M0,96L34.3,90.7C68.6,85,137,75,206,58.7C274.3,43,343,21,411,21.3C480,21,549,43,617,96C685.7,149,754,235,823,256C891.4,277,960,235,1029,218.7C1097.1,203,1166,213,1234,234.7C1302.9,256,1371,288,1406,304L1440,320L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path>
       </svg>
       <div className={`flex flex-col justify-center w-full space-y-8 relative py-16`} ref={ref}>
         <div className={`h-14 ${isDarkMode ? `text-light-800` : `text-paletteText-primary`} font-bold text-center text-2xl sm:text-3xl`} ref={firstRef}>
@@ -76,7 +76,7 @@ const Honors = forwardRef(function Honors(props: any, ref: React.Ref<HTMLDivElem
           {_.map(honors, (item: any, key: any) => (
             <div 
               key={"HONOR-" + key}
-              className="flex flex-col md:flex-row space-x-0 md:space-x-4"
+              className="flex flex-col gap-y-4 md:flex-row space-x-0 md:space-x-4"
             >
               <a 
                 href={item.link} 
