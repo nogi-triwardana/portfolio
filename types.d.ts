d833212788024bd3e2b54fa045d3d1852fe76791
Nogi Ragil Triwardana <nogi.triwardana@ifg-life.id>
2025-06-20 10:44:21 +0700

    feat(type): add custom utility type

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,16 @@
 declare module '*.pdf';
+
+export type ReturnFuncType<T> = T extends (...args: unknown[]) => infer R ? R : never;
+
+export type Kebab2Snake<T> = T extends `${infer R}-${infer K}` ? `${R}_${K}` : false;
+
+export type KeyValueSplitter<T extends string> = T extends `${infer K}:${infer V}`
+  ? {
+      key: K;
+      value: V;
+    }
+  : never;
+
+export type UserTransformed<T extends Record<string, unknown>> = {
+  [K in keyof T as `get${Capitalize<K & string>}`]: () => T[K];
+};
